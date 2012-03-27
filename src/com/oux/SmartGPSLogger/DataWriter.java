d24e93e47dc73004bb5141e3516d99b151537eb9
Jeremy Compostella <jeremy.compostella@gmail.com>
2012-03-28 00:05:35 +0200

    main: remove log in writer

--- src/com/oux/SmartGPSLogger/DataWriter.java
+++ src/com/oux/SmartGPSLogger/DataWriter.java
@@ -77,12 +77,5 @@ public class DataWriter
         writer.flush();         // TODO: Flush on each line is fine
                                 // but use a BufferedWriter to flush
                                 // on each line is ugly
-
-        Log.d("DateWriter", DateFormat.format("yyyy:MM:dd", loc.getTime()) +
-              "," + DateFormat.format("hh:mm:ss", loc.getTime()) +
-              "," + loc.getLatitude() +
-              "," + loc.getLongitude() +
-              "," + loc.getSpeed() +
-              "," + loc.getAltitude() + " wroten");
     }
 }
