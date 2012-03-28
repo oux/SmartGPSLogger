f3186a396f18905ac54c458f89eb2a534365f069
Sébastien MICHEL <oux@falguerolles.org>
2012-03-29 01:08:21 +0200

    Change target api for real target

--- mkproj.sh
+++ mkproj.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 # android list target to get target version
 [ -d src ] && ACTION=update || { ACTION=create;OPTS="-k com.oux.$(basename $(pwd)) -a $(basename $(pwd))"; }
-android $ACTION project --name $(basename $(pwd)) --target 1 --path $(pwd) $OPTS
+android $ACTION project --name $(basename $(pwd)) --target android-10 --path $(pwd) $OPTS
